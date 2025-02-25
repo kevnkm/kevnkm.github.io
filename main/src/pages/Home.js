78c09c9d0e4ad991d344ac0021da18e1a59c6c50
Kevin Kim <kk946@cornell.edu>
2025-02-25 19:54:31 +0900

    Plain JavaScript → React

---
+++ main/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="relative w-full h-[calc(100vh-4rem)] bg-white flex justify-center">
+      <div className="container mx-auto p-4 w-full max-w-md sm:max-w-2xl lg:max-w-3xl"></div>
+    </div>
+  );
+}
+
+export default Home;
