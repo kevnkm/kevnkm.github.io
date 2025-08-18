73e6121540985386688afff2b251b482bac121c3
Kevin Kim <kk946@cornell.edu>
2025-08-18 16:07:08 +0900

    Update MasonryGrid.tsx

--- main/src/components/MasonryGrid.tsx
+++ main/src/components/MasonryGrid.tsx
@@ -8,7 +8,7 @@ interface MasonryGridProps {
 
 const MasonryGrid: React.FC<MasonryGridProps> = ({ items }) => {
     return (
-        <div className="w-full max-w-7xl mx-auto px-4 py-8">
+        <div className="w-full max-w-7xl mx-auto px-4 py-2">
             <div className="masonry-wrapper">
                 {items.map((item) => (
                     <MasonryCard key={item.id} item={item} />
