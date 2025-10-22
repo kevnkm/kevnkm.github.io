31bee676749534c8476c222454d4a4545212c5c1
Kevin Kim <kk946@cornell.edu>
2025-10-23 08:44:50 +0900

    Update App.tsx

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { About } from "./pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
