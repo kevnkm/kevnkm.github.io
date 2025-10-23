c69872bc225c615e03372f09ac3b89434d30ad2b
Kevin Kim <kk946@cornell.edu>
2025-10-23 14:51:35 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { About } from "./pages/About";
 import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </HashRouter>
   );
 }
